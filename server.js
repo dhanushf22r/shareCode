e4b1e8dd03987db4b992f14ab12d173b1d6dc7cb
dhanushf22r <56345238+dhanushf22r@users.noreply.github.com>
2023-07-27 20:24:27 +0100

    comments

--- server.js
+++ server.js
@@ -27,10 +27,20 @@ db.once("open", () => {
 
   // Define routes and handlers
   app.get("/", (req, res) => {
-    const code = `Welcome to WasteBin!
+    const code = `Welcome to Share-Code (or any other text!)
 
-Use the commands in the top right corner
-to create a new file to share with others.`;
+    To begin, click on 'new' and then just paste the code here, 
+    as if you were feeding a hungry baby bird. Then, with great 
+    anticipation, click on the 'Save' button, like you're saving 
+    the last piece of cake from a ravenous horde of dessert thieves. 
+    And voila! Share the unique URL with your friend, who will be 
+    so impressed that they'll probably start calling you the 
+    'Master of Code Sharing.' They'll click the link, eyes wide 
+    with wonder, and witness the code/text in all its magnificent 
+    glory. Just make sure they don't mistake it for an ancient 
+    magical incantation and accidentally summon a unicorn or 
+    something. 
+    Happy code sharing! `;
 
     res.render("code-display", { code, language: "plaintext" });
   });
